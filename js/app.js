6b500ea03e5da8c258b221f1a760a82a62ebcd72
Mason Embry <masonembry81@mac.com>
2016-09-18 16:02:00 -0700

    showRangeOfStudents function added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
-// Store number of .student-list items (students) in a variable
-var numberOfStudents = $(".student-list li").length;
+var students = $(".student-list li");
+var studentsPerPage = 10;
 
-// If numberOfStudents is > 10 add paginationDiv
-if (numberOfStudents > 10) {
+// Store number of students in a variable.
+var numberOfStudents = students.length;
+
+// If numberOfStudents is > 10 add paginationDiv.
+if (numberOfStudents > studentsPerPage) {
 	var $paginationDiv = $('<div class="pagination"></div>');
 	$paginationDiv.append("<ul></ul>");
-	var necessaryPages = Math.ceil(numberOfStudents / 10); // Divides numberOfStudents by 10 then rounds up to next integer
+	var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage); // Divides numberOfStudents by 10 then rounds up to next integer.
+	
+	// Add a li to the paginationDiv ul for every necessary page.
 	for (var i = 1; i < necessaryPages; i++) {
-		$paginationDiv.append('<li><a href="#">' + i + "</a></li>");
+		$paginationDiv.append('<li><a href="#" class="page-link-' + i + '">' + i + "</a></li>");
 	}
 	$(".student-list").append($paginationDiv);
 }
+
+// Show a range of students
+var showRangeOfStudents = function(low, high) {
+	students.slice(low, high).show();
+}
+
+// Hide all students then load first page of students.
+students.hide();
+
+
+
+
+
+
 
