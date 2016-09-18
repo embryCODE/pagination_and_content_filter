96f33158275056e20a837ba3ab2198c8753e6d3e
Mason Embry <masonembry81@mac.com>
2016-09-18 16:59:10 -0700

    selectPageOfStudents now working.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var students = $(".student-list li");
 var studentsPerPage = 10;
+var $rangeIndicator = $('<p id="range-indicator"></p>')
 
 // Store number of students in a variable.
 var numberOfStudents = students.length;
@@ -19,11 +20,25 @@ if (numberOfStudents > studentsPerPage) {
 
 // Show a range of students
 var showRangeOfStudents = function(low, high) {
-	students.slice(low, high).show();
+	students.slice(low, (high + 1)).show();
 }
+
+var selectPageOfStudents = function(pageNumber) {
+	lastStudentToShow = pageNumber * 10;
+	firstStudentToShow = lastStudentToShow - 9;
+	showRangeOfStudents((firstStudentToShow - 1), (lastStudentToShow -1));
+	
+	// Update range indicator
+	$rangeIndicator.append("Showing student number " + firstStudentToShow + " through " + lastStudentToShow + ".");
+	$(".page-header").append($rangeIndicator);
+}
+
+// Create search div
+
 
 // Hide all students then load first page of students.
 students.hide();
+selectPageOfStudents(1);
 
 
 
