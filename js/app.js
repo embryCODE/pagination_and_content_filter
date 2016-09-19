0f2a9f53810269797c7c391670b272d2f627d8ec
Mason Embry <masonembry81@mac.com>
2016-09-18 21:31:35 -0700

    Much cleaner pagination creation.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 var $students = $(".student-list li");
 var numberOfStudents = $students.length;
 var studentsPerPage = 10;
+var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage); // Necessary pages to fit all students
 
 
 
 // Show a range of students
 var showRangeOfStudents = function(low, high) {
-	$students.hide(); // Hides all students first, just in case.
+	$students.hide(); // Hides all students first.
 	$students.slice(low, (high + 1)).show();
 }
 
-// Select a page of students
-var selectPageOfStudents = function(pageNumber) {
-	lastStudentToShow = pageNumber * studentsPerPage;
-	firstStudentToShow = lastStudentToShow - (studentsPerPage - 1);
-	showRangeOfStudents((firstStudentToShow - 1), (lastStudentToShow -1));
-}
-
+// Create pagination div
 var createPaginationDiv = function() {
-	console.log('pagination created');
+	var $paginationDiv = $('<div class="pagination"><ul></ul></div>') // Creates pagination div with empty ul.
+	var ulContent = ''; // Creates empty string for building ul content in loop
+
+	for (var i = 0; i < necessaryPages; i++) {
+		ulContent += "<li><a>" + (i + 1) + "</a></li>"; // Builds html of ul, incrementing the link text
+	}
+	
+	$paginationDiv.find("ul").append(ulContent);
+	$(".student-list").append($paginationDiv);
 }
 
-// Hide all students then load first page of students.
-$students.hide();
+
 
 // If numberOfStudents is > 10 add paginationDiv.
 if (numberOfStudents > studentsPerPage) {
 	createPaginationDiv();
 }
-
-selectPageOfStudents(1);
 
 
 
