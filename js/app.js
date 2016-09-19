88bbe46db465492174f1928df3de6b96fea96aa9
Mason Embry <masonembry81@mac.com>
2016-09-19 12:46:13 -0700

    Search field is sorta working.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var $students = $(".student-list li"); // Total students regardless of search selection.
-var searchSelected = $students; // Total students selected by search. Initialized as total students.
+var searchResults = $students; // Total students selected by search. Initialized as total students.
 var studentsPerPage = 10; // Entered here as variable so it can be changed if desired.
+var $studentName = $(".student-details h3");
+var $studentEmail = $(".email");
 
 
 
@@ -18,7 +20,7 @@ $(".page-header").append($searchDiv);
 var createPaginationDiv = function() {
 	
 	// Calculate number of pages necessary based on students in search results and students per page.
-	var numberOfStudents = searchSelected.length;
+	var numberOfStudents = searchResults.length;
 	var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage);
 	
 	// Initialize new elements to build pagination div.	
@@ -57,7 +59,7 @@ var createPaginationDiv = function() {
 // Displays students with necessary pagination
 var displayStudents = function(startingStudent) {
 	$students.hide(); // Hides all students first.
-	$students.slice(startingStudent, (startingStudent + studentsPerPage)).show();
+	searchResults.slice(startingStudent, (startingStudent + studentsPerPage)).show();
 }
 
 // Selects page by adding .active class and calls showRangeOfStudents with starting student
@@ -73,9 +75,25 @@ var setActivePage = function(e) {
 	displayStudents(startingStudent);
 }
 
+// Search button event listener
+$(".student-search button").click(function() {
+	var $foundPerson = $(""); 
+	var searchInputText = $(".student-search input").val().toLowerCase();
+	$students.each(function() {
+		var studentNameText = $(this).find($studentName).text().toLowerCase();
+		var studentEmailText = $(this).find($studentEmail).text().toLowerCase();
+		if (studentNameText.indexOf(searchInputText) !== -1 || studentEmailText.indexOf(searchInputText) !== -1) {
+			console.log("found it");
+			$foundPerson = $foundPerson.add(this);
+		}
+	});
+	searchResults = $foundPerson;
+	displayStudents(0);
+});
 
 
-// Create pagination div. Will not create if searchSelected is <= 10.
+
+// Create pagination div. Will not create if searchResults is <= 10.
 createPaginationDiv();
 
 // Show initial page of students
