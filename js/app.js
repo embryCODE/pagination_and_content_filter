55bc8322f0b68bcc4a63cd7a3270194a019ccff8
Mason Embry <masonembry81@mac.com>
2016-09-19 11:03:11 -0700

    Pagination div created in a better way.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
-var $students = $(".student-list li");
-var numberOfStudents = $students.length;
-var studentsPerPage = 10;
-var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage); // Necessary pages to fit all students
+var $students = $(".student-list li"); // Total students regardless of search selection.
+var searchSelected = $students; // Total students selected by search. Initialized as total students.
+var studentsPerPage = 10; // Entered here as variable so it can be changed if desired.
 
 
+// Create pagination div
+var createPaginationDiv = function() {
+	
+	// Calculate number of pages necessary based on students in search results and students per page.
+	var numberOfStudents = searchSelected.length;
+	var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage);
+	
+	// Initialize new elements to build pagination div.	
+	var $paginationDiv = $('<div class="pagination"></div>')
+	var $paginationUl = $("<ul></ul>");
+	var $paginationLi;
+	var $paginationLink;
+	
+	// No pagination div created if only one page necessary.
+	if (necessaryPages > 1) {
+		
+		// Create ul outside of loop.
+		$paginationDiv.append($paginationUl);
+		
+		// Loop to create li and a.
+		for (var i = 0; i < necessaryPages; i++) {
+			$paginationLi = $("<li></li>");
+			$paginationLink = $('<a href=#>' + (i + 1) + '</a>')
+			$paginationLi.append($paginationLink);
+			$paginationUl.append($paginationLi);
+			
+			// Set click handler on each link 
+			$paginationLink.click(setActivePage);
+		}
+		
+		$(".student-list").append($paginationDiv);
+	}
+}
 
 // Show a range of students
 var showRangeOfStudents = function(low, high) {
 	$students.hide(); // Hides all students first.
 	$students.slice(low, high).show();
-}
-
-// Create pagination div
-var createPaginationDiv = function() {
-	var $paginationDiv = $('<div class="pagination"><ul></ul></div>') // Creates pagination div with empty ul.
-	var ulContent = ''; // Creates empty string for building ul content in loop
-
-	for (var i = 0; i < necessaryPages; i++) {
-		ulContent += "<li><a href=#>" + (i + 1) + "</a></li>"; // Builds html of ul, incrementing the link text
-	}
-	
-	$paginationDiv.find("ul").append(ulContent);
-	$(".student-list").append($paginationDiv);
-}
-
-// Set .active class on chosen page
-var setActivePage = function(activePage) {
-	$(".pagination ul li a").removeClass("active"); // First remove .active class from all page links
-	$(".pagination ul li a").eq(activePage - 1).addClass("active"); // "activePage - 1" to adjust for zero-based index
 }
 
 // "Turn the page" to the page indicated by pagination click handler
@@ -47,22 +60,25 @@ var turnThePage = function(page) {
 	showRangeOfStudents(startIndex, endIndex);
 }
 
-
-
-// If numberOfStudents is > studentsPerPage add paginationDiv and set first page link to .active
-if (numberOfStudents > studentsPerPage) {
-	createPaginationDiv();
-	setActivePage(1);
-	turnThePage(1);
-}
-
-// Pagination click handler
+// Set .active link
+var setActivePage = function(activePage) {
+	// Pagination click handler
 $(".pagination ul li a").on("click", function(e) {
 	e.preventDefault();
 	var whichPageLinkClicked = parseInt($(this).text());
 	setActivePage(whichPageLinkClicked);
 	turnThePage(whichPageLinkClicked);
 });
+	$(".pagination ul li a").removeClass("active"); // First remove .active class from all page links
+	$(".pagination ul li a").eq(activePage - 1).addClass("active"); // "activePage - 1" to adjust for zero-based index
+}
+
+
+createPaginationDiv();
+setActivePage(1);
+turnThePage(1);
+
+
 
 
 
