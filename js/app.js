38899b90817da6ee3fdb24edd7e8abffa1052469
Mason Embry <masonembry81@mac.com>
2016-09-18 20:28:51 -0700

    Deleted failed code.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,8 @@
 var $students = $(".student-list li");
 var numberOfStudents = $students.length;
 var studentsPerPage = 10;
-var $rangeIndicator = $('<p id="range-indicator"></p>')
 
 
-// Create pagination div
-var createPaginationDiv = function() {
-	var $paginationDiv = $('<div class="pagination"></div>');
-	$paginationDiv.append("<ul></ul>");
-	var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage); // Divides numberOfStudents by 10 then rounds up to next integer.
-	
-	// Add a list item to the paginationDiv ul for every necessary page.
-	for (var i = 1; i < necessaryPages; i++) {
-		$paginationDiv.find('ul').append('<li><a href="#" class="page-link">' + i + "</a></li>");
-	}
-	$(".student-list").append($paginationDiv);
-}
 
 // Show a range of students
 var showRangeOfStudents = function(low, high) {
@@ -28,19 +15,11 @@ var selectPageOfStudents = function(pageNumber) {
 	lastStudentToShow = pageNumber * studentsPerPage;
 	firstStudentToShow = lastStudentToShow - (studentsPerPage - 1);
 	showRangeOfStudents((firstStudentToShow - 1), (lastStudentToShow -1));
-	
-	// Add .active class to selected page
-	$(".pagination ul li:nth-child(" + pageNumber + ")").find('a').addClass("active");
-	
-	// Update range indicator
-	$rangeIndicator.append("Showing student number " + firstStudentToShow + " through " + lastStudentToShow + ".");
-	$(".page-header").append($rangeIndicator);
 }
 
-// When I click on a pagination li a
-	// Call selectPageOfStudents([correct page number])
-
-// Create search div
+var createPaginationDiv = function() {
+	console.log('pagination created');
+}
 
 // Hide all students then load first page of students.
 $students.hide();
@@ -51,6 +30,7 @@ if (numberOfStudents > studentsPerPage) {
 }
 
 selectPageOfStudents(1);
+
 
 
 
