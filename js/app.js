94c63a2d10e1c8fc708b3e7fb333a588fe2adefc
Mason Embry <masonembry81@mac.com>
2016-09-18 20:21:38 -0700

    Dead end. Pagination links are too hacky.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,55 @@
-var students = $(".student-list li");
+var $students = $(".student-list li");
+var numberOfStudents = $students.length;
 var studentsPerPage = 10;
 var $rangeIndicator = $('<p id="range-indicator"></p>')
 
-// Store number of students in a variable.
-var numberOfStudents = students.length;
 
-// If numberOfStudents is > 10 add paginationDiv.
-if (numberOfStudents > studentsPerPage) {
+// Create pagination div
+var createPaginationDiv = function() {
 	var $paginationDiv = $('<div class="pagination"></div>');
 	$paginationDiv.append("<ul></ul>");
 	var necessaryPages = Math.ceil(numberOfStudents / studentsPerPage); // Divides numberOfStudents by 10 then rounds up to next integer.
 	
-	// Add a li to the paginationDiv ul for every necessary page.
+	// Add a list item to the paginationDiv ul for every necessary page.
 	for (var i = 1; i < necessaryPages; i++) {
-		$paginationDiv.append('<li><a href="#" class="page-link-' + i + '">' + i + "</a></li>");
+		$paginationDiv.find('ul').append('<li><a href="#" class="page-link">' + i + "</a></li>");
 	}
 	$(".student-list").append($paginationDiv);
 }
 
 // Show a range of students
 var showRangeOfStudents = function(low, high) {
-	students.slice(low, (high + 1)).show();
+	$students.hide(); // Hides all students first, just in case.
+	$students.slice(low, (high + 1)).show();
 }
 
+// Select a page of students
 var selectPageOfStudents = function(pageNumber) {
-	lastStudentToShow = pageNumber * 10;
-	firstStudentToShow = lastStudentToShow - 9;
+	lastStudentToShow = pageNumber * studentsPerPage;
+	firstStudentToShow = lastStudentToShow - (studentsPerPage - 1);
 	showRangeOfStudents((firstStudentToShow - 1), (lastStudentToShow -1));
+	
+	// Add .active class to selected page
+	$(".pagination ul li:nth-child(" + pageNumber + ")").find('a').addClass("active");
 	
 	// Update range indicator
 	$rangeIndicator.append("Showing student number " + firstStudentToShow + " through " + lastStudentToShow + ".");
 	$(".page-header").append($rangeIndicator);
 }
 
+// When I click on a pagination li a
+	// Call selectPageOfStudents([correct page number])
+
 // Create search div
 
-
 // Hide all students then load first page of students.
-students.hide();
+$students.hide();
+
+// If numberOfStudents is > 10 add paginationDiv.
+if (numberOfStudents > studentsPerPage) {
+	createPaginationDiv();
+}
+
 selectPageOfStudents(1);
 
 
